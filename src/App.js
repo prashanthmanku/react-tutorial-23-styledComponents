9e26900f16c8ae5f5da698aa5ee0cb3733b6a283
prashanth <prashanthmanku97@gmail.com>
2023-06-17 00:52:20 +0000

    styled

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Heading, CustomButton} from './StyledComponents'
+import './App.css'
+
+const App = () => (
+  <>
+    <Heading>Hello world</Heading>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
+      Click
+    </CustomButton>
+  </>
+)
+
+export default App
